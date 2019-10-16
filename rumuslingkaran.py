874f104cd78557add4b921a97a6fa0a23e128188
iinmahgafhira07 <49119136+iinmahgafhira07@users.noreply.github.com>
2019-10-16 00:58:16 -0700

    Create rumuslingkaran.py

---
+++ rumuslingkaran.py
@@ -0,0 +1,17 @@
+import math
+
+
+def menghitungluaslingkaran(r):
+     return math.pi * r * r
+     
+          
+def kelilinglingkaran(r):
+     return 2 * math.pi * r
+     
+          
+def volumelingkaran(r):
+     return math.pi * r * r
+     
+     
+def diameterlingkaran(r):
+     return 2 * r
